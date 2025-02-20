0fe64f306c37a805986171315e9a70ba7d931ea6
FFCfelps1 <163139810+FFCfelps1@users.noreply.github.com>
2025-02-20 17:16:35 -0300

    Matrix exercicio 6

---
+++ Matrixces.java
@@ -0,0 +1,25 @@
+public class Main {
+
+  public static void main(String[] args) {  
+    exercise6(args);
+    //exercise7(args);
+    //exercise8(args);
+    //exercise9(args);
+    //exercise10(args);
+  }
+  //Exercise 6 do a program  with any matrix is possible to clean all the positions and than put -1 in all the positions
+  public static void exercise6(String[] args) {
+    int[][] matrix = new int[3][3];
+    int rows = matrix.length;
+    int columns = matrix[0].length;
+    
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        matrix[i][j] = -1;
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+}
--- Vectors.java
+++ Vectors.java
@@ -3,7 +3,9 @@ public class Main {
   public static void main(String[] args) {  
     //exercise1(args);
     //exercise2(args);
-    exercise3(args);
+    //exercise3(args);
+    //exercise4(args);
+    //exercise5(args);
   }
 
   // Exercise 1 do a program to include 100 numbers in a list and print them in the console
@@ -46,12 +48,37 @@ public class Main {
   // Exercise 4 do a program to include the letters of the alphabet in a list "A" to "Z" and them change one letter and one after, ex: A -> B, B -> A
   public static void exercise4(String[] args) {
     char[] alphabet = new char[26];
-    char letter = 'A';
+    char letter = 'B';
 
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < 13; i++) {
       alphabet[i] = letter;
       System.out.println(alphabet[i]);
-      letter++;
+      letter --;
+      alphabet[i] = letter;
+      System.out.println(alphabet[i]);
+      letter += 3;
     }
+  }
+
+  // Exercise 5 do a program to include the numbers from 0 to 15 in a list and them look for 4 numbers is the same rigth to left and left to rigth, ex: 1221
+  public static void exercise5(String[] args) {
+      int[] numbers = new int[17];
+
+      System.out.print('[');
+      for (int i = 0; i <= 15; i++) {
+        //Put the numbers in the list in random(0, 9)
+          numbers[i] = (int) (Math.random() * 10);
+          System.out.print(numbers[i]+ " ");
+      }
+      System.out.print(']');
+
+      for (int i = 0; i < 16; i+= 4){
+        if (numbers[i] == numbers[i + 3] && numbers[i + 1] == numbers[i + 2]) {
+          System.out.println("The numbers are the same from right to left and left to right");
+          System.out.println(numbers[i] + "" + numbers[i + 1] + "" + numbers[i + 2] + "" + numbers[i + 3]);
+        }
+      }
+
+      System.out.println("The numbers are not the same from right to left and left to right");
   }
 }
