6c3c356d133587a37f36deba1dcb31ccf1935c0e
FFCfelps1 <felipefazio.costa@gmail.com>
2025-02-20 09:20:46 -0300

    exercise 4 java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
-  public static void main(String[] args) {
-    exercise1(args);
-    exercise2(args);
+  public static void main(String[] args) {  
+    //exercise1(args);
+    //exercise2(args);
+    exercise3(args);
   }
 
   // Exercise 1 do a program to include 100 numbers in a list and print them in the console
@@ -27,6 +28,30 @@ public class Main {
       } else {
         System.out.println(numbers[i]);
       }
+    }
+  }
+
+  // Exercise 3 do a program to include the letters of the alphabet in a list "A" to "Z" and print them in the console
+  public static void exercise3(String[] args) {
+    char[] alphabet = new char[26];
+    char letter = 'A';
+
+    for (int i = 0; i < 26; i++) {
+      alphabet[i] = letter;
+      System.out.println(alphabet[i]);
+      letter++;
+    }
+  }
+
+  // Exercise 4 do a program to include the letters of the alphabet in a list "A" to "Z" and them change one letter and one after, ex: A -> B, B -> A
+  public static void exercise4(String[] args) {
+    char[] alphabet = new char[26];
+    char letter = 'A';
+
+    for (int i = 0; i < 26; i++) {
+      alphabet[i] = letter;
+      System.out.println(alphabet[i]);
+      letter++;
     }
   }
 }
