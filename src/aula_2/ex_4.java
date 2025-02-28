dd7abe3971ac52ee9b6f1ca2fe779fd677c462c6
FFCfelps1 <163139810+FFCfelps1@users.noreply.github.com>
2025-02-28 08:49:30 -0300

    Update ex_4.java

--- src/aula_2/ex_4.java
+++ src/aula_2/ex_4.java
@@ -3,33 +3,23 @@ import java.util.Random;
 public class MatrixSymmetryChecker {
 
     public static void main(String[] args) {
-        int[] sizes = {10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000};
-        Random random = new Random();
+        int[] parametros = {10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000};
 
-        for (int n : sizes) {
-            int[][] matrix = generateMatrix(n, random);
-
+        for (int n = 0; n<parametros.length; n+=2) {
             long startTime = System.nanoTime();
+            
+            int[][] matrix = new int [parametros[tamanho]][parametros[tamanho + 1]];
             boolean isSymmetric = checkSymmetry(matrix);
+            
             long endTime = System.nanoTime();
 
             System.out.println("Matrix size: " + n + "x" + n);
             System.out.println("Symmetric: " + isSymmetric);
-            System.out.println("Time taken: " + (endTime - startTime) / 1_000_000.0 + " ms");
+            System.out.println("Time taken: " + (endTime - startTime));
             System.out.println("--------------------------------");
         }
     }
-
-    private static int[][] generateMatrix(int n, Random random) {
-        int[][] matrix = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = random.nextInt(100); // Valores de 0 a 99
-            }
-        }
-        return matrix;
-    }
-
+    
     private static boolean checkSymmetry(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < n; i++) {
