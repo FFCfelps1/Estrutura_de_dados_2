a113243c0d8fa376fe4c06738256bc50f6f02b2f
FFCfelps1 <felipefazio.cossta@gmail.com>
2025-02-27 21:03:28 -0300

    ex_3-aula_2

--- src/aula_2/ex_2.java
+++ src/aula_2/ex_2.java
@@ -1,12 +1,20 @@
-public class ex_2{
-    public static void main(String[] args){
+public class ex_2 {
+    public static void main(String[] args) {
         int[] parametros = {10, 50, 100, 500, 1000, 5000, 10000, 100000, 500000};
-        for (int tamanho : parametros){
-            int[] array_test = new int[parametros[tamanho]];
-            int[] array_copy = new int[parametros[tamanho]];
-            for (int j : array_test){
+        for (int tamanho = 0; tamanho < parametros.length; tamanho++) {
+            long startTime = System.nanoTime(); // Captura o tempo inicial
+
+            int[] array_test = new int[tamanho];
+            int[] array_copy = new int[tamanho];
+            for (int j = 0; j < array_test.length; j++) {
                 array_copy[j] = array_test[j];
             }
+
+            long endTime = System.nanoTime(); // Captura o tempo final
+            long timeElapsed = endTime - startTime; // Calcula o tempo decorrido
+
+            // Exibe o tempo de execução
+            System.out.println("Tempo de execução para tamanho " + tamanho + " em Nanosegundos: " + timeElapsed);
         }
     }
 }
--- src/aula_2/ex_3.java
+++ src/aula_2/ex_3.java
@@ -1,3 +1,27 @@
 public class ex_3 {
-    
+    public static void main(String[] args) {
+        int[] parametros = {10, 10, 50, 75, 100, 300, 500, 200, 1000, 1000, 5000, 7000, 10000, 1, 50000, 25000, 100000, 100000, 500000, 1000};
+        for (int tamanho = 0; tamanho < parametros.length; tamanho += 2) {
+            long startTime = System.nanoTime(); // Captura o tempo inicial
+
+            // Criar uma matriz, baseado em valores dos parametros
+            double matriz[][] = new double[parametros[tamanho]][parametros[tamanho + 1]];
+            // Limpar a matriz e colocar em todas as posições o valor -1
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    matriz[i][j] = -1;
+                }
+            }
+
+            long endTime = System.nanoTime(); // Captura o tempo final
+            long timeElapsed = endTime - startTime; // Calcula o tempo decorrido
+
+            // Exibe o tempo de execução
+            System.out.println("Tempo de execução para tamanho " + tamanho + " em Nanosegundos: " + timeElapsed);
+
+            // Liberar memória
+            matriz = null;
+            System.gc();
+        }
+    }
 }
